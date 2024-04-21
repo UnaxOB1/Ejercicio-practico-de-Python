cc28cde6ee929559aa168417b1f655038fcebaf6
unaxortt <33076375-unaxortt@users.noreply.replit.com>
2024-04-21 19:17:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#1
+python_string = 'This is for testing purposes'
+python_number = 42
+python_list = [6, 'Blue', False, 1.5, 'Orange']
+python_boolean = True
+
+#2
+first_letters = python_string[0:3]
+
+#3
+first_element = python_list[0]
+
+#4
+new_python_number = python_number + 10
+
+#5
+last_element = python_list[-1]
+
+#6
+names = 'harry,alex,susie,jared,gail,conner'
+
+list_of_names = names.split(',')
+
+#7
+first_word = python_string[0:4].upper()
+
+new_python_string = first_word + python_string[4:]
+
+#8
+notification = f'You have {python_number} unread message/s!'
+
+#9
+print('hello world')
